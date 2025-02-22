d5f5ef66183e8ee6ec3222886372b76a84541fbb
Flora Taima <florataima@gmail.com>
2025-02-22 18:06:13 +0100

    made changes from coach sam

--- blogtask/models.py
+++ blogtask/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class post (models.Model):
+class Post (models.Model):
     author = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
     text = models.TextField()
